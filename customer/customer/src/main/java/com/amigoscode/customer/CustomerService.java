01d56fb39fe9119bdbd2156f60a7a0c45e6a74bd
zeinab.montazeri <zeinab.montazeri@gmail.com>
2023-01-11 13:55:58 +0330

    customer service talk to fraud service by FeignClient

--- customer/customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ customer/customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -1,5 +1,7 @@
 package com.amigoscode.customer;
 
+import com.amigoscode.clients.fraud.FraudCheckResponse;
+import com.amigoscode.clients.fraud.FraudClient;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -9,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
+    private final FraudClient fraudClient;
     private final RestTemplate restTemplate;
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
@@ -20,12 +23,8 @@ public class CustomerService {
         //todo: check if email valid
         //todo: check if email not taken
         customerRepository.saveAndFlush(customer);
-        //todo: check if fraudster
-        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://FRAUD/api/v1/fraud-check/{customerId}",
-                FraudCheckResponse.class,
-                customer.getId()
-        );
+
+        FraudCheckResponse fraudCheckResponse = fraudClient.isFraudster(customer.getId());
 
         if (fraudCheckResponse.isFraudster()) {
             throw new IllegalStateException("fraudster");
